0348d031bffcc16e31f20956359c8e36308ce2ad
VanshikaP <vanshikapundir92@gmail.com>
2020-03-04 10:08:07 -0800

    resolved merge conflicts

--- secret-recipes/src/index.js
+++ secret-recipes/src/index.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -14,7 +6,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
->>>>>>> c6f35ff297df0456290a0679600c119e8a2f47c5
 
 // REDUCER IMPORT
 import combineReducers from "./redux";
